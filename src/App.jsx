99a5cd9f340511bd7de589e1cbb9caedc550df61
Lucas Kaoru <lucasnohara51@gmail.com>
2026-08-31 19:15:47 -0300

    chore: criando projeto inicial

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div>
+            <h1>EcoTrend</h1>
+            <p>Ecobags para um mundo mais sustentável.</p>
+        </div>
+    );
+}
+
+export default App;
