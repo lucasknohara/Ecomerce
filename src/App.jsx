87ad7d73f3f0583de4df248362b1038157409092
Lucas Kaoru <lucasnohara51@gmail.com>
2026-08-31 19:22:35 -0300

    feat: carregando produtos com fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,60 @@
+import { useEffect, useState } from "react";
+
 function App() {
+    const [produtos, setProdutos] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function buscarProdutos() {
+            try {
+                setLoading(true);
+
+                const resposta = await fetch("/products.json");
+
+                if (!resposta.ok) {
+                    throw new Error("Erro ao carregar produtos");
+                }
+
+                const dados = await resposta.json();
+
+                setProdutos(dados);
+            } catch (error) {
+                console.log("Erro:", error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        buscarProdutos();
+    }, []);
+
     return (
         <div>
             <h1>EcoTrend</h1>
-            <p>Ecobags para um mundo mais sustentável.</p>
+
+            {loading ? (
+                <p>Carregando produtos...</p>
+            ) : (
+                <div>
+                    {produtos.map((produto) => (
+                        <div key={produto.id}>
+                            <img
+                                src={produto.imagem}
+                                alt={produto.nome}
+                                width="200"
+                            />
+
+                            <h2>{produto.nome}</h2>
+
+                            <p>{produto.descricao}</p>
+
+                            <strong>
+                                R$ {produto.preco.toFixed(2)}
+                            </strong>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
